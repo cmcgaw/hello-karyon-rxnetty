0b6186f52b299640c273074e10fa6cca5e8446c8
cmcgaw <cmcgaw@gmail.com>
2018-09-26 23:39:27 -0400

    Update postInstall.sh

--- pkg_scripts/postInstall.sh
+++ pkg_scripts/postInstall.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+#setup google app creds
+export GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/[FILE_NAME].json"
+
 # create log folder
 install  --mode=755 --directory  /var/log/hello-karyon-rxnetty
+
+
 
 # start services
 start hello-karyon-rxnetty
